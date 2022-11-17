2a31afce3063b8f39f581c169fe4ece266d8ecde
RohabAmir <rohabamir2@gmail.com>
2022-11-17 21:55:01 +0500

    First commit

---
+++ 1929_ConcatenationOfArrays.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def getConcatenation(self,nums):
+#Sol;1
+        ans1=[] 
+        ans2=[]
+        for i in range(len(nums)):
+            ans1.append(nums[i])
+        for i in range(len(nums)):
+            ans2.append(nums[i])
+        return ans1 + ans2
+# Sol2;
+        n=len(nums)
+        ans=2*nums
+        for i in range(len(nums)):
+            ans[i]==nums[i]
+            ans[i+n]==nums[i]
+        return ans
+r=Solution()
+print(r.getConcatenation([1,2,1]))     
+            
+        
+    
+        
+    
+            
