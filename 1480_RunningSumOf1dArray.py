db04a4df2b0c3f371d007fc23ecafb129d093baf
RohabAmir <rohabamir2@gmail.com>
2022-11-18 18:22:09 +0500

    Done

---
+++ 1480_RunningSumOf1dArray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def runningSum(self, nums):
+        sum=0
+        ans=1*nums #create a new array having size equal to nums arrays
+        for i in range(len(nums)):
+            sum= sum+nums[i]
+            ans[i]=sum
+        return ans
+r=Solution()
+print(r.runningSum([1,2,3,4]))
