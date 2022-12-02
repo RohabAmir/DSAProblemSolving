5ca0f0083597ff7d1f2986dcaa40e53979ea010d
RohabAmir <rohabamir2@gmail.com>
2022-12-02 22:54:15 +0500

    Done

--- 1290_CovertBinaryNoInALinkedListToInteger.py
+++ 1290_CovertBinaryNoInALinkedListToInteger.py
@@ -7,8 +7,10 @@ class Solution(object):
     def getDecimalValue(self, head):
         temp=head #pointing at the head of linkedlist
         x=0
+#traversing over the entire linkedlist
         while temp is not None:
             x+=temp.val
-            x*=10
+            x*=2
             temp=temp.next
-        return x/10
+        return x/2
+    
